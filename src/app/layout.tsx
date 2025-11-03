a615375e69b1d62a6e32e727f6412a3fe0f37576
lsbjordao <tucarj@gmail.com>
2025-11-03 17:54:01 -0300

    google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -7,7 +8,7 @@ export const metadata = {
   title: "TTS-Mimosa",
   icons: {
     icon: "/TTS-Mimosa-App/favicon.png",
-  }
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +18,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" className="dark">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-5P7S18CDTY"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-5P7S18CDTY');
+          `}
+        </Script>
+      </head>
       <body
         className={`${inter.className} flex h-screen overflow-hidden bg-black text-white`}
       >
